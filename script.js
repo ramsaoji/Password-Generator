f361a3e0b0aa20b41d985a504821e82dcc9ef07b
ramsaoji <mygithub@techness.in>
2022-10-19 02:35:32 +0530

    password validation and onCheckbox generation

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ options = document.querySelectorAll(".option input"),
 passwordInput = document.querySelector(".input-box input"),
 copyIcon = document.querySelector(".input-box span"),
 passIndicator = document.querySelector(".pass-indicator"),
-generateBtn = document.querySelector(".generate-btn");
+generateBtn = document.querySelector(".generate-btn"),
+excludeDuplicateCheckbox = document.getElementById("exc-duplicate"),
+upperCaseChekbox = document.getElementById("uppercase"),
+numbersChekbox = document.getElementById("numbers"),
+symbolsChekbox = document.getElementById("symbols");
+
 
 const characters = { //object of letters,numbers & symbols
     lowercase : "abcdefghijklmnopqrstuvwxyz",
     uppercase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numbers : "0123456789",
-    symbols : "!@#$%^&*-_+?~"
+    symbols : "!@#$%^&*-_+?~",
+    lowercaseRegex : "(?=.*[a-z])",
+    uppercaseRegex : "(?=.*[A-Z])",
+    numbersRegex : "(?=.*\\d)",
+    symbolsRegex : "(?=.*[!@#$%^&*-_+?~])",
+    spaceRegex : "(?=.*[ ])",
+    prefixRegexPattern : "^",
+    suffixRegexPattern : ".+$"
 }
 
 //Generating random password
@@ -17,11 +29,41 @@ const generatePassword = () => {
 
     let staticPassword = "",
     randomPassword = "",
-    excludeDuplicate = false;
-    passLength = lengthSlider.value;
+    excludeDuplicate = false,
+    passLength = lengthSlider.value,
+    regex = characters.prefixRegexPattern,
+    regexExp = "";
 
     options.forEach(option => { //looping through each option's checkbox
+        
+        
         if(option.checked){ // if checkbox is checked
+
+            if(upperCaseChekbox.checked == true && passLength <= 25){
+                excludeDuplicateCheckbox.disabled = false;
+            }else if(passLength <= 25 && numbersChekbox.checked == true && symbolsChekbox.checked == true){
+                excludeDuplicateCheckbox.disabled = false;
+            }
+            else if(upperCaseChekbox.checked == true && passLength > 25 && numbersChekbox.checked == true && symbolsChekbox.checked == true){
+                excludeDuplicateCheckbox.disabled = false;
+            }else{
+                excludeDuplicateCheckbox.checked = false;
+                excludeDuplicateCheckbox.disabled = true;
+            }
+
+            if(option.id == "lowercase"){
+                regex += characters.lowercaseRegex;
+            } else if(option.id == "uppercase"){
+                regex += characters.uppercaseRegex;
+            } else if(option.id == "numbers"){
+                regex += characters.numbersRegex;
+            } else if(option.id == "symbols"){
+                regex += characters.symbolsRegex;
+            } else if(option.id == "spaces"){
+                regex += characters.spaceRegex;
+            }
+
+            regexExp = new RegExp(regex);
 
             if(option.id !== "exc-duplicate" && option.id !== "spaces"){
                 staticPassword += characters[option.id]; //adding a particular key value from characters object to staticPassword
@@ -31,21 +73,38 @@ const generatePassword = () => {
                 excludeDuplicate = true;
             }
         }
-    })
+        
+    });
+
+    regex += characters.suffixRegexPattern;
 
     for(let i = 0; i < passLength; i++){
 
-        randomPassword += staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        randomPassword = randomPassword + staticPassword[Math.floor(Math.random() * staticPassword.length)];
     }
 
     //if excludeDuplicate is true then get unique values from randomPassword
     if(excludeDuplicate){
+
         randomPassword = (Array.from(new Set(randomPassword.split("")))).join("");
+
+        if(randomPassword.length < passLength) {
+            generatePassword();
+            return
+        }
     }
 
-    passwordInput.value = randomPassword; //passing randomPassword to passwordInput field
+    //PASSWORD VALIDATOR
+    // console.log("before -" + randomPassword);
+    if(regexExp.test(randomPassword)){
+        passwordInput.value = randomPassword;
+    } else {
+        generatePassword();
+        return
+    }
 
 }
+
 
 const updatePassIndicator = () => {
     //if lengthSlider value is less than 8 then pass "weak" as passIndicator id else if lengthSlider value is less than 16 then pass "medium" as id else pass "strong" as id
@@ -54,9 +113,17 @@ const updatePassIndicator = () => {
 
 //Updating password length slider value to the password length (span) value.
 const updateSlider = () => {
+
+    rangeValue = lengthSlider.value;
+    if(rangeValue < 5){
+        lengthSlider.value = 5;
+        return
+    }
+
     document.querySelector(".details span").innerHTML = lengthSlider.value;
     generatePassword();
     updatePassIndicator();
+    
 }
 updateSlider();
 
@@ -67,6 +134,14 @@ const copyPassword = () => {
     setTimeout(() => { //after 1.5 sec changing tick icon back to copy icon
         copyIcon.innerHTML = "copy_all";
     }, 1500);
+}
+
+//Calling generatePassword on every checkbox selected
+for (var i = 0; i < options.length; i++) {
+    if (options[i].type === 'checkbox') {
+
+        options[i].onclick = generatePassword;
+    }
 }
 
 copyIcon.addEventListener("click", copyPassword);
